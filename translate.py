2f8e2b53ba46df09b349f96f3ee979daab9944a4
intelburn <nerdacs@gmail.com>
2018-04-04 15:45:33 -0400

    Added translate.py for translation between UDP and TCP OpenPixelControl platforms

---
+++ translate.py
@@ -0,0 +1,9 @@
+import socket
+outside = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+inside = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+outside.bind(("0.0.0.0", 7890))
+inside.connect(("127.0.0.1",7890))
+while True:
+    data, addr = outside.recvfrom(65000)
+    print("received {1} bytes of data from {0}".format(addr, len(data)))
+    inside.send(data)
