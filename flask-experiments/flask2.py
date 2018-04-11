7b30bbea31a4b87136d76669f5ebcd95e883408a
intelburn <nerdacs@gmail.com>
2018-04-11 15:38:52 -0400

    Remove old experiments with Python Flask

--- flask-experiments/flask2.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/')
-def yo(name=None):
-    return render_template('flask2.html')
-
-if __name__ == "__main__":
-    app.run(ssl_context='adhoc')
-
